5fbe0399aeba52ddea30116a80599e8977dc663d
hisreal <65047346+hisreal@users.noreply.github.com>
2026-04-01 19:17:44 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -105,7 +105,7 @@
                             <li><a href="index.php">Home</a></li>
                             <li><a href="about.php">About</a></li>
                             <li><a href="services.php">Services</a></li>
-                            <li><a href="contact.html">Contact</a></li>
+                            <li><a href="contact.php">Contact</a></li>
                         </ul>
                     </div>
                 </div>
